fb32f30068e01149cbfeb9496c3b9f4f95501502
nifontpro <nifontbus@yandex.ru>
2022-03-23 19:47:27 +0300

    1.1 Основная настройка модулей и зависимостей

---
+++ buildSrc/src/main/java/Compose.kt
@@ -0,0 +1,29 @@
+object Compose {
+    const val composeVersion = "1.1.0"
+    const val composeCompilerVersion = "1.1.0"
+    const val material = "androidx.compose.material:material:$composeVersion"
+    const val ui = "androidx.compose.ui:ui:$composeVersion"
+    const val uiGraphics = "androidx.compose.ui:ui-graphics:$composeVersion"
+    const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview:$composeVersion"
+    const val runtime = "androidx.compose.runtime:runtime:$composeVersion"
+    const val compiler = "androidx.compose.compiler:compiler:$composeCompilerVersion"
+    const val materialIcon = "androidx.compose.material:material-icons-extended:$composeVersion"
+
+    private const val hiltNavigationComposeVersion = "1.0.0"
+    const val hiltNavigationCompose = "androidx.hilt:hilt-navigation-compose:$hiltNavigationComposeVersion"
+
+    private const val activityComposeVersion = "1.4.0"
+    const val activityCompose = "androidx.activity:activity-compose:$activityComposeVersion"
+
+    private const val lifecycleVersion = "2.4.0"
+    const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion"
+
+    private const val coilVersion = "1.4.0"
+    const val coilCompose = "io.coil-kt:coil-compose:$coilVersion"
+
+    private const val permissionVersion = "0.24.3-alpha"
+    const val permission = "com.google.accompanist:accompanist-permissions:$permissionVersion"
+
+    private const val splashVersion = "1.0.0-beta01"
+    const val splashScreen = "androidx.core:core-splashscreen:$splashVersion"
+}
